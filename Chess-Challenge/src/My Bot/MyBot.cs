2cfc3c0bfab45ee8704415725ce48fb705bdc65d
Jacoder23 <Jacoder23@users.noreply.github.com>
2025-12-15 14:31:41 +0800

    negamax and eval actually fixed (and many debug tools added)

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,6 +7,7 @@ public class MyBot : IChessBot
 {
     const bool debug = true;
     const bool verbose = false;
+    const bool clearConsoleOnMove = true;
     const int maxDepth = 2;
     const int infinity = 10000;
     int nodesSearched = 0;
@@ -14,60 +15,101 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
+        if (clearConsoleOnMove)
+            Console.Clear();
+
         Move[] rootMoves = board.GetLegalMoves();
         Move nextMove = rootMoves[0];
-        int score = 0;
-        int alpha = -infinity; // the highest score that max player (us) can guarantee thus far, upper bound
-        int beta = infinity; // the lowest score that min player (them) can guarantee thus far, lower bound
-        int moveScore = 0;
+        int score = -infinity;
+        int alpha = -infinity; // the highest score that max player (us) can guarantee thus far
+        int beta = infinity; // the lowest score that min player (them) can guarantee thus far
         nodesSearched = 0;
 
         if (debug)
-            Console.WriteLine(board.CreateDiagram() + "\n- - - - - - - - - - - - - - - - ");
+            Console.WriteLine(board.CreateDiagram());
 
-        // searching children of root node
-        foreach (Move move in rootMoves)
-        {
-            board.MakeMove(move);
-            moveScore = -NegaMax(maxDepth - 1, - beta, -alpha);
-
-            if(debug && verbose)
-                Console.WriteLine(board.CreateDiagram() + "\nEval: " + moveScore);
-            else if (debug)
-                Console.WriteLine("\n" + board.GetFenString() + "\nEval: " + moveScore);
-
-            board.UndoMove(move);
-
-            if (moveScore > score)
-            {
-                score = moveScore;
-                nextMove = move;
-            }
-        }
+        NegaMax(maxDepth, -beta, -alpha);
 
         int NegaMax(int depth, int alpha, int beta)
         {
-            if (depth == 0)
-                return Evaluate(board);
-
             Move[] moves = board.GetLegalMoves();
+
+            if (depth == 0 || moves.Length == 0)
+            {
+                int eval = 0;
+                bool isMate = board.IsInCheckmate();
+
+                if (isMate)
+                {
+                    if (board.IsWhiteToMove)
+                    {
+                        eval = 10000 - (maxDepth - depth);
+                    }
+                    else if (!board.IsWhiteToMove)
+                    {
+                        eval = -10000 + (maxDepth - depth);
+                    }
+                }
+                else if (board.IsDraw())
+                {
+                    eval = 0;
+                }
+                else
+                {
+                    eval = Evaluate(board);
+                }
+
+                if (debug && verbose)
+                    Console.WriteLine("- - - - - - - - - - - - - - - - " + "\nLeaf node" + "  | Eval: " + eval + (isMate ? " (Checkmate)" : "") + "\n" + board.CreateDiagram());
+                else if (debug)
+                    Console.WriteLine("- - - - - - - - - - - - - - - - " + "\nLeaf node" + "  | Eval: " + eval + (isMate ? " (Checkmate)" : "") + "\n" + board.GetFenString());
+
+                return eval; // not propogating back properly
+            }
+
             int moveScore = 0;
             foreach (Move move in moves)
             {
                 board.MakeMove(move);
                 nodesSearched++;
+                if (debug)
+                    Console.WriteLine("                        " + move.ToString() + " at ply of " + (maxDepth - depth + 1));
                 moveScore = -NegaMax(depth - 1, -beta, -alpha);
+                if(debug && depth == maxDepth)
+                    Console.WriteLine("                        Root Child: " + move.ToString() + " |  " + moveScore + " |  " + "Next move: " + nextMove.ToString());
                 board.UndoMove(move);
 
                 if (moveScore >= beta) // its already guaranteed that the min player won't go down this line
                 {
                     nodesPruned++;
-                    break;
+                    if (debug)
+                        Console.WriteLine("                        Node pruned. Score was " + moveScore + " and beta was " + beta + ".");
+                    return beta;
                 }
 
-                alpha = Math.Max(alpha, moveScore);
+                if (moveScore > score && depth == maxDepth)
+                {
+                    nextMove = move;
+                    score = moveScore;
+                }
+
+                if (Math.Abs(moveScore) == 9998)
+                    Console.WriteLine("Alpha is " + alpha);
+
+                if (moveScore > alpha)
+                {
+                    alpha = moveScore;
+                }
+
+                if(depth == maxDepth)
+                {
+                    if (debug && verbose)
+                        Console.WriteLine(board.CreateDiagram() + move.ToString() + "\nEval: " + moveScore);
+                    else if (debug)
+                        Console.WriteLine("\n" + board.GetFenString() + "\n" + move.ToString() + "\nEval: " + moveScore);
+                }
             }
-            return moveScore;
+            return alpha;
         }
 
         if (debug)
@@ -86,27 +128,10 @@ public class MyBot : IChessBot
         int[] pieceValues = { 100, 310, 330, 510, 880, 10000 };
         int eval = 0;
 
-        if (board.IsInCheckmate())
-        {
-            if (board.IsWhiteToMove)
-            {
-                return -10000;
-            }
-            else if (board.IsWhiteToMove)
-            {
-                return 10000;
-            }
-        }
-        else if (board.IsDraw())
-        {
-            return 0;
-        }
-
         foreach (var pieceList in board.GetAllPieceLists())
         {
             foreach (var piece in pieceList)
             {
-                int index = piece.IsWhite ? piece.Square.File + piece.Square.Rank * 8 : (piece.Square.File) + (7 - piece.Square.Rank) * 8;
                 int mult = piece.IsWhite ? 1 : -1;
                 switch (piece.PieceType)
                 {
